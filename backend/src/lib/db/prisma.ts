d3b9005d9566ac4637af8e2d6b788cb9fbb4eead
chinmareno <chinmareno1@gmail.com>
2026-01-12 22:14:50 +0700

    refactor: change to monorepo

---
+++ backend/src/lib/db/prisma.ts
@@ -0,0 +1,5 @@
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client/extension";
+
+// const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+// const prisma = new PrismaClient({ adapter: pool });
