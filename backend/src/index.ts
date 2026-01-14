11f28f9c966e71af64837c22c3be6dc8a3bf7456
chinmareno <chinmareno1@gmail.com>
2026-01-14 19:00:25 +0700

    chore: setup json & cookie parser + make path to src by @/*

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT! || 3000;
+
+// Parse JSON bodies
+app.use(express.json());
+
+// Parse cookies
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
