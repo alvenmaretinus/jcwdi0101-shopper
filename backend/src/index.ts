b960fbc792c92332520e015f99542080c92bae7d
chinmareno <chinmareno1@gmail.com>
2026-01-14 18:24:59 +0700

    chore: remove NEXT_PUBLIC in env name

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT! || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
