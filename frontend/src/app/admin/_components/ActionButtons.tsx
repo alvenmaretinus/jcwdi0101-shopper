781caf848850d34eedc5c2dfa8acfdaa8867e32f
chinmareno <chinmareno1@gmail.com>
2026-01-16 23:14:50 +0700

    feat(admin): add ActionButtons for cancel and submit button group

---
+++ frontend/src/app/admin/_components/ActionButtons.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  isSubmitting: boolean;
+  onSubmit: () => void;
+  cancelText?: string;
+  submitText?: string;
+};
+export const FormActions = ({
+  isSubmitting,
+  cancelText = "Cancel",
+  onSubmit,
+  submitText = "Submit",
+}: Props) => {
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-end gap-4">
+      <Button variant="secondary" onClick={() => router.back()}>
+        {cancelText}
+      </Button>
+      <Button disabled={isSubmitting} onClick={onSubmit}>
+        {submitText}
+      </Button>
+    </div>
+  );
+};
