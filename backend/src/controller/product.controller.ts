60f865ba25aa5c1f66068cd1f29ccf7fd83dacb4
michaelchristopher-ui <michael.christopher@ui.ac.id>
2026-01-21 07:34:32 +0700

    feat2: [SHP-69] added super admin auth for some APIs

--- backend/src/controller/product.controller.ts
+++ backend/src/controller/product.controller.ts
@@ -5,13 +5,15 @@ import { ProductService } from '../service/product/product.service';
 import { CreateProductInput, CreateProductSchema, GetProductByIdInput, 
     GetProductByIdSchema,  FilterInput, GetProductsByFilterSchema, 
     GetProductsByFilterInput, UpdateProductSchema } from '../schema/product';
+import { isAuth } from '../middleware/isAuth';
+import { isSuperAdmin } from '../middleware/isSuperAdmin';
 
 const productsRepo = new PrismaRepository(prisma);
 const productService = new ProductService(productsRepo);
 
 const router = Router();
 
-router.get("/products", async (req, res) => {
+router.get("/products",  async (req, res) => {
     const inputData: GetProductsByFilterInput = GetProductsByFilterSchema.parse(req.query);
     const filter: FilterInput = inputData.filter;
     const result = await productService.getProductsByFilterWithOptionalStock(filter, inputData.withStock);
@@ -24,20 +26,20 @@ router.get("/product/:id", async (req, res) => {
     return res.json(result);
 });
 
-router.post("/product", async (req, res) => {
+router.post("/product", isAuth, isSuperAdmin, async (req, res) => {
     const inputData: CreateProductInput = CreateProductSchema.parse(req.body);
     const createdProduct = await productService.createProduct(inputData);
     return res.status(201).json(createdProduct);
 });
 
-router.patch("/product/:id", async (req, res) => {
+router.patch("/product/:id", isAuth, isSuperAdmin, async (req, res) => {
     const { id } = req.params;
     const data = UpdateProductSchema.parse({ id, ...req.body });
     const updatedProduct = await productService.updateProduct(id, data);
     return res.json(updatedProduct);
 });
 
-router.delete("/product/:id", async (req, res) => {
+router.delete("/product/:id", isAuth, isSuperAdmin, async (req, res) => {
     const inputData: GetProductByIdInput = GetProductByIdSchema.parse(req.params);
     await productService.deleteProduct(inputData.id);
     return res.status(204).send();
