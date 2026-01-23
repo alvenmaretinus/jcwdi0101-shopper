9fb583d8193ac2354eca37fc1800249e25b2cb4c
michaelchristopher-ui <michael.christopher@ui.ac.id>
2026-01-23 07:13:31 +0700

    feat2: [SHP-72] registered the product stores route

--- backend/src/route/index.ts
+++ backend/src/route/index.ts
@@ -3,6 +3,7 @@ import storeRouter from "./store";
 import userRouter from "./user";
 import authRouter from "./auth";
 import productRouter from "./product";
+import productStoreRouter from "./productstore";
 
 export const appRouter = Router();
 
@@ -11,3 +12,4 @@ appRouter.use(storeRouter);
 appRouter.use(userRouter);
 appRouter.use(authRouter);
 appRouter.use(productRouter);
+appRouter.use(productStoreRouter);
