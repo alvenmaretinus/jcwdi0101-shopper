a4a3f35c17a0d0bd8856e9a776afbcce6908d07a
michaelchristopher-ui <michael.christopher@ui.ac.id>
2026-01-29 07:30:12 +0700

    feat2: [SHP-78] added admin role checking and storeId input on isAuth

--- backend/src/controller/sales-report.controller.ts
+++ backend/src/controller/sales-report.controller.ts
@@ -6,14 +6,20 @@ import { SalesReportRepository } from "../repository/sales-report/interface"
 import { PrismaRepository } from "../repository/sales-report/adapter_prisma"
 import { prisma } from "../lib/db/prisma"
 import { SalesReportEntity } from "../repository/sales-report/entities"
+import { isAuth } from "../middleware/isAuth"
+import { isAdmin } from "../middleware/isAdmin"
+import { UserRole } from "../../prisma/generated/browser"
 
 const router = Router()
 
 const salesReportRepository: SalesReportRepository= new PrismaRepository(prisma)
 const salesReportService: Service = new SalesReportService(salesReportRepository)
 
-router.get("/", async (req, res) => {
+router.get("/", isAuth, isAdmin, async (req, res) => {
     const inputData: GetSalesReportByFilterInput = GetSalesReportByFilterSchema.parse(req.query)
+    if (req.user!.role == UserRole.ADMIN && inputData.storeId !== req.user!.storeId) {
+        return res.status(403).json({ message: "Forbidden" })
+    }
     const result: SalesReportEntity[] = await salesReportService.getSalesReportByFilter(inputData)
     return res.json(result)
 })
