08326b9e34ace0a949f9ab66f5bb0c46cc4e0ed2
Michael Christopher Manullang <manullang.c@gmail.com>
2026-02-02 19:45:53 +0700

    feat: [SHP-77] renamed input files

--- backend/src/schema/discount/index.ts
+++ backend/src/schema/discount/index.ts
@@ -1,6 +1,6 @@
 import {CreateDiscountSchema, CreateDiscountInput } from './CreateDiscountSchema';
 import {UpdateDiscountSchema, UpdateDiscountInput} from './UpdateDiscountSchema'    ;
-import { GetDiscountByIdSchema, GetDiscountByIdInput} from './GetDiscountByIdSchema';
+import {GetDiscountByIdSchema, GetDiscountByIdInput} from './GetDiscountByIdSchema';
 import {GetDiscountsByFilterSchema, GetDiscountsByFilterInput} from './GetDiscountsByFilterSchema';
 import {DeleteDiscountByIdSchema, DeleteDiscountByIdInput} from './DeleteDiscountByIdSchema';
 
