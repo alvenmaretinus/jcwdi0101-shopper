98ca58f9535e87823273fbb7cb7f703c3730d0fe
Michael Christopher Manullang <manullang.c@gmail.com>
2026-02-04 18:06:55 +0700

    feat: [SHP-77] minor formatting corrections on discount index.ts

--- backend/src/schema/discount/index.ts
+++ backend/src/schema/discount/index.ts
@@ -1,8 +1,8 @@
 import {CreateDiscountSchema, CreateDiscountInput } from './CreateDiscountSchema';
-import {UpdateDiscountSchema, UpdateDiscountInput} from './UpdateDiscountSchema'    ;
-import {GetDiscountByIdSchema, GetDiscountByIdInput} from './GetDiscountByIdSchema';
-import {GetDiscountsByFilterSchema, GetDiscountsByFilterInput} from './GetDiscountsByFilterSchema';
-import {DeleteDiscountByIdSchema, DeleteDiscountByIdInput} from './DeleteDiscountByIdSchema';
+import {UpdateDiscountSchema, UpdateDiscountInput } from './UpdateDiscountSchema';
+import {GetDiscountByIdSchema, GetDiscountByIdInput } from './GetDiscountByIdSchema';
+import {GetDiscountsByFilterSchema, GetDiscountsByFilterInput } from './GetDiscountsByFilterSchema';
+import {DeleteDiscountByIdSchema, DeleteDiscountByIdInput } from './DeleteDiscountByIdSchema';
 
 export {
     CreateDiscountSchema, CreateDiscountInput
