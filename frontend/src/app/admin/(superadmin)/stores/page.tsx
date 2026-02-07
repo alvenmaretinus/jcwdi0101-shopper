ef3395ca4589d64d58b361440d4d53ef87d95c56
chinmareno <chinmareno1@gmail.com>
2026-02-08 00:06:49 +0700

    feat: add pagination, sorting, filtering for store

--- frontend/src/app/admin/(superadmin)/stores/page.tsx
+++ frontend/src/app/admin/(superadmin)/stores/page.tsx
@@ -1,10 +1,5 @@
-import { getStores } from "@/services/store/getStores";
 import { Stores } from "./_components/Stores";
-import { headers } from "next/headers";
 
-export default async function StoresPage() {
-  const nextHeaders = await headers();
-  const stores = await getStores(nextHeaders);
-
-  return <Stores stores={stores} />;
+export default function StoresPage() {
+  return <Stores />;
 }
