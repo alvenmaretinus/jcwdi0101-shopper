972e5fd07817583196fd6d3d1cc8f77df32eebd2
chinmareno <chinmareno1@gmail.com>
2026-02-07 13:45:13 +0700

    feat: redir to admin if admin visit home

--- frontend/src/app/(user)/(home)/page.tsx
+++ frontend/src/app/(user)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import AdminRedirect from "./_components/AdminRedirect";
 import { CategorySection } from "./_components/Home/CategorySection";
 import { HeroSection } from "./_components/Home/HeroSection";
 import { ProductSection } from "./_components/Home/ProductSection";
@@ -6,6 +7,7 @@ import { ReferralCodeModal } from "./_components/Home/ReferralCodeModal";
 export default function HomePage() {
   return (
     <>
+      <AdminRedirect />
       <HeroSection />
       <CategorySection />
       <ProductSection />
