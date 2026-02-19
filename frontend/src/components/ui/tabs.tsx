33e73030682472292861db07ee02a07cda52a74f
Michael Christopher Manullang <manullang.c@gmail.com>
2026-02-19 19:13:45 +0700

    feat2: fix error on tabs

--- frontend/src/components/ui/tabs.tsx
+++ frontend/src/components/ui/tabs.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
-<<<<<<< HEAD
-import { Tabs as TabsPrimitive } from "radix-ui";
-=======
 import * as TabsPrimitive from "@radix-ui/react-tabs";
->>>>>>> main
 
 import { cn } from "@/lib/utils";
 
@@ -31,11 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-<<<<<<< HEAD
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm bg-transparent px-3 py-1.5 text-sm font-medium text-foreground/70 ring-offset-background transition-all hover:text-foreground [&[data-state=active]]:bg-background [&[data-state=active]]:text-foreground [&[data-state=active]]:shadow-sm [&[data-state=active]]:font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-=======
       "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
->>>>>>> main
       className,
     )}
     {...props}
@@ -58,8 +50,4 @@ const TabsContent = React.forwardRef<
 ));
 TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-<<<<<<< HEAD
 export { Tabs, TabsList, TabsTrigger, TabsContent };
-=======
-export { Tabs, TabsList, TabsTrigger, TabsContent };
->>>>>>> main
